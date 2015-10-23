35a71042c60668e018280ef2f772ee0b8a5568e8
Per-Victor Persson <dracyr@gmail.com>
2015-10-23 10:12:16 +0200

    Cleaned up music file finder

--- lib/find_music_files.ex
+++ lib/find_music_files.ex
@@ -4,24 +4,26 @@ defmodule GrooveLion.FindMusicFiles do
 
   def add_files do
     find_files("/home/dracyr/Music")
-    |> Enum.each(fn(file) ->
-      if is_nil Repo.get_by(Track, filename: file) do
-        {:ok, metadata} = File.read!(file) |> GrooveLion.ID3v2.parse()
-        track = %Track{title: metadata.title, artist: metadata.artist, filename: file}
-
-        case Repo.insert(track) do
-          {:ok, _track} ->
-            IO.puts "Added: " <> file
-          {:error, changeset} ->
-            IO.puts "Error adding: " <> file
-        end
-      end
-    end)
+    |> Enum.each(fn(file) -> add_file(file) end)
   end
 
   def find_files(path) do
     path
     |> Path.join("**/*.{mp3}")
     |> Path.wildcard()
+  end
+
+  defp add_file(file) do
+    if is_nil Repo.get_by(Track, filename: file) do
+      {:ok, metadata} = File.read!(file) |> GrooveLion.ID3v2.parse()
+      track = %Track{title: metadata.title, artist: metadata.artist, filename: file}
+
+      case Repo.insert(track) do
+        {:ok, _track} ->
+          IO.puts "Added: " <> file
+        {:error, changeset} ->
+          IO.puts "Error adding: " <> file
+      end
+    end
   end
 end
