0b92f8a0f324f21bb268dd2ce7a3068897437710
Per-Victor Persson <dracyr@gmail.com>
2016-02-08 22:51:25 +0100

    Update phoenix and add it to npm dependencies

--- mix.exs
+++ mix.exs
@@ -30,10 +30,10 @@ defmodule SummerSinger.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.1.0"},
+    [{:phoenix, "~> 1.1.4"},
      {:phoenix_ecto, "~> 2.0"},
      {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.3"},
+     {:phoenix_html, "~> 2.5"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
      {:cowboy, "~> 1.0"},
